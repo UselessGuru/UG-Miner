44e781139cfd596f124830d029bdaa64623ecb2a
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2024-01-02 08:30:14 +0100

    v6.0.0
    Initial release

---
+++ Web/scripts/demo.ps1
@@ -0,0 +1,28 @@
+<#
+Copyright (c) 2018-2023 UselessGuru
+
+UG-Miner is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+UG-Miner is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+#>
+
+<#
+Product:        UG-Miner
+File:           demo.psm1
+Version:        6.0.0
+Version date:   20 October 2022
+#>
+
+# Try running this script as:  http://localhost:3999/scripts/demo.ps1?message=Hello%20World!
+
+param($Parameters)
+Write-Output $Parameters.message
