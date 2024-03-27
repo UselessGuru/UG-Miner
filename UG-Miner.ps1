a34ca4b37869ccc681568a95b1bd723e0c33f350
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2024-03-27 17:29:36 +0100

    Update UG-Miner.ps1

--- UG-Miner.ps1
+++ UG-Miner.ps1
@@ -296,7 +296,7 @@ $Variables.Branding = [PSCustomObject]@{
     BrandName    = "UG-Miner"
     BrandWebSite = "https://github.com/UselessGuru/UG-Miner"
     ProductLabel = "UG-Miner"
-    Version      = [System.Version]"6.2.1"
+    Version      = [System.Version]"6.2.2"
 }
 
 $WscriptShell = New-Object -ComObject Wscript.Shell
